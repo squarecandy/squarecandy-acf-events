3c4ba12b7be9128c14a9cea6026b5b992e093f26
Peter Wise <peter@squarecandy.net>
2020-05-30 21:54:27 -0400

    refactor: change to standardized file strucutre

--- functions.php
+++ functions.php
@@ -1,16 +1,8 @@
 <?php
-/*
-Plugin Name: Square Candy ACF Events
-Plugin URI:  https://github.com/squarecandy/squarecandy-acf-events
-GitHub Plugin URI:  https://github.com/squarecandy/squarecandy-acf-events
-Description: A custom events plugin that using Advanced Custom Fields
-Version:	 1.2.1
-Author:	  Peter Wise
-Author URI:  http://squarecandy.net
-Text Domain: squarecandy-acf-events
-*/
+// main plugin file
 
 define( 'ACF_EVENTS_DIR_PATH', plugin_dir_path( __FILE__ ) );
+define( 'ACF_EVENTS_URL', plugin_dir_url( __FILE__ ) );
 
 // don't let users activate w/o ACF
 register_activation_hook( __FILE__, 'squarecandy_acf_events_activate' );
@@ -29,8 +21,8 @@ function squarecandy_acf_events_activate() {
 // Front End Scripts and Styles
 function squarecandy_acf_events_enqueue_scripts() {
 
-	wp_enqueue_style( 'squarecandy-fontawesome', plugins_url( 'css/vendor/font-awesome/css/font-awesome.min.css', __FILE__ ) );
-	wp_enqueue_style( 'squarecandy-acf-events-css', plugins_url( 'css/squarecandy-acf-events.css', __FILE__ ) );
+	wp_enqueue_style( 'squarecandy-fontawesome', ACF_EVENTS_URL . 'dist/css/vendor/font-awesome/css/font-awesome.min.css', false, 'version-1.0.0' );
+	wp_enqueue_style( 'squarecandy-acf-events-css', ACF_EVENTS_URL . 'dist/css/main.min.css', false, 'version-1.0.0' );
 
 	if (
 		// if maps option is on
@@ -40,11 +32,11 @@ function squarecandy_acf_events_enqueue_scripts() {
 		// and it's a single page view
 		is_single() &&
 		// and it's an event
-		'event' == get_post_type( get_the_ID() )
+		'event' === get_post_type( get_the_ID() )
 	) {
 		$google_maps_api_key = get_field( 'google_maps_api_key', 'option' );
-		wp_enqueue_script( 'squarecandy-acf-events-gmapapi', 'https://maps.googleapis.com/maps/api/js?key=' . $google_maps_api_key, array(), '20180101', true );
-		wp_enqueue_script( 'squarecandy-acf-events-maps', plugins_url( 'js/googlemaps.js', __FILE__ ), array( 'jquery' ), false, true );
+		wp_enqueue_script( 'squarecandy-acf-events-gmapapi', 'https://maps.googleapis.com/maps/api/js?key=' . $google_maps_api_key, array(), 'version-1.0.0', true );
+		wp_enqueue_script( 'squarecandy-acf-events-maps', ACF_EVENTS_URL . 'dist/js/googlemaps.min.js', array( 'jquery' ), 'version-1.0.0', true );
 		// gather data to localize in the google maps script
 		$data['location']   = get_field( 'venue_location' );
 		$data['mapjson']    = get_field( 'google_maps_json', 'option' );
@@ -61,13 +53,13 @@ function squarecandy_acf_events_enqueue_scripts() {
 		wp_localize_script( 'squarecandy-acf-events-maps', 'DATA', $data );
 	}
 
-	wp_enqueue_script( 'squarecandy-acf-events-js', plugins_url( 'js/squarecandy-acf-events.js', __FILE__ ), array( 'jquery' ), false, true );
+	wp_enqueue_script( 'squarecandy-acf-events-js', ACF_EVENTS_URL . 'dist/js/main.min.js', array( 'jquery' ), 'version-1.0.0', true );
 }
 add_action( 'wp_enqueue_scripts', 'squarecandy_acf_events_enqueue_scripts' );
 
 // Admin Scripts and Styles
 function squarecandy_acf_events_admin_enqueue() {
-	wp_enqueue_style( 'squarecandy-acf-events-admin-css', plugins_url( 'css/squarecandy-acf-events-admin.css', __FILE__ ), false, '1.0.0' );
+	wp_enqueue_style( 'squarecandy-acf-events-admin-css', ACF_EVENTS_URL . 'dist/css/admin.min.css', false, 'version-1.0.0' );
 }
 add_action( 'admin_enqueue_scripts', 'squarecandy_acf_events_admin_enqueue' );
 
@@ -99,13 +91,13 @@ function squarecandy_acf_events_template_chooser( $template ) {
 	$post_id = get_the_ID();
 
 	// If this is an event post type and is a single post display, show custom template
-	if ( 'event' == get_post_type( $post_id ) && is_single() ) {
+	if ( 'event' === get_post_type( $post_id ) && is_single() ) {
 
 		// Check if a custom template exists in the theme folder, if not, load the plugin template file
-		if ( $theme_file = locate_template( array( 'event-single.php' ) ) ) {
-			$file = $theme_file;
+		if ( locate_template( array( 'event-single.php' ) ) ) {
+			$file = locate_template( array( 'event-single.php' ) );
 		} else {
-			$file = dirname( __FILE__ ) . '/templates/event-single.php';
+			$file = ACF_EVENTS_DIR_PATH . '/templates/event-single.php';
 		}
 
 		// allow other plugins to override via the squarecandy_acf_events_single_template filter
@@ -114,12 +106,12 @@ function squarecandy_acf_events_template_chooser( $template ) {
 	}
 
 	// if this is the archive page, show custom archive template
-	if ( 'event' == get_post_type( $post_id ) && is_archive() ) {
+	if ( 'event' === get_post_type( $post_id ) && is_archive() ) {
 		// Check if a custom template exists in the theme folder, if not, load the plugin template file
-		if ( $theme_file = locate_template( array( 'archive-events.php' ) ) ) {
-			$file = $theme_file;
+		if ( locate_template( array( 'archive-events.php' ) ) ) {
+			$file = locate_template( array( 'archive-events.php' ) );
 		} else {
-			$file = dirname( __FILE__ ) . '/templates/archive-events.php';
+			$file = ACF_EVENTS_DIR_PATH . '/templates/archive-events.php';
 		}
 
 		// allow other plugins to override via the squarecandy_acf_events_archive_template filter
@@ -131,64 +123,121 @@ function squarecandy_acf_events_template_chooser( $template ) {
 
 
 function get_squarecandy_acf_events_date_display( $event, $compact = null ) {
+
+	// timedate formats and separators
 	$formats = get_field( 'date_formats', 'option' );
 	$sep     = '<span class="datetime-sep">' . $formats['datetime_sep'] . '</span>';
 	$sep2    = '<span class="datetime-sep">' . $formats['datetime_sep2'] . '</span>';
 	$range   = '<span class="datetime-range">' . $formats['datetime_range'] . '</span>';
+
+	// ensure all required fields have a value
+	$multi_day  = $event['multi_day'] ?? false;
+	$all_day    = $event['all_day'] ?? false;
+	$start_date = $event['start_date'] ?? false;
+	$end_date   = $event['end_date'] ?? false;
+	$start_time = $event['start_time'] ?? false;
+	$end_time   = $event['end_time'] ?? false;
 
 	if ( $compact ) {
 		$formats['date_format']             = $formats['date_format_compact'];
 		$formats['date_format_multi_start'] = $formats['date_format_compact_multi_start'];
 		$formats['date_format_multi_end']   = $formats['date_format_compact_multi_end'];
 	}
+
 	$output = '';
-	if ( $event['multi_day'] != 1 ) {
+
+	if ( ! $multi_day ) {
 		// single date
-		$output .= date( $formats['date_format'], strtotime( $event['start_date'] ) );
-		if ( $event['all_day'] != 1 ) {
+		// example: June 3, 2019
+		$output .= date_i18n( $formats['date_format'], strtotime( $start_date ) );
+		if ( ! $all_day ) {
 			// with time
-			$output .= $sep . '<span class="time">' . date( $formats['time_format'], strtotime( $event['start_time'] ) ) . '</span>';
+			// example: June 3, 2019 - 3:00pm
+			$output .= $sep . '<span class="time">' . date_i18n( $formats['time_format'], strtotime( $start_time ) ) . '</span>';
 		}
 	} else {
 		// multi day
 		if (
 			// all day is checked, start and end date are different, and end date is not empty
-			( $event['all_day'] == 1 && $event['start_date'] != $event['end_date'] && ! empty( $event['end_date'] ) ) ||
-			// all day not checked, start and end date are different, and end date is not empty, both start and end times are empty
-			( $event['all_day'] != 1 && $event['start_date'] != $event['end_date'] && ! empty( $event['end_date'] ) && empty( $event['start_time'] ) && empty( $event['end_time'] ) )
+			(
+				$all_day &&
+				$start_date !== $end_date &&
+				! empty( $end_date )
+			) ||
+			// OR all day not checked, start and end date are different,
+			// and end date is not empty, both start and end times are empty
+			(
+				! $all_day &&
+				$start_date !== $end_date &&
+				! empty( $end_date ) &&
+				empty( $start_time ) &&
+				empty( $end_time )
+			)
 		) {
 			// range of dates with no time (all day)
-			$output .= date( $formats['date_format_multi_start'], strtotime( $event['start_date'] ) );
+			// Example: June 20 — July 4, 2019
+			$output .= date_i18n( $formats['date_format_multi_start'], strtotime( $start_date ) );
 			$output .= $range;
-			$output .= date( $formats['date_format_multi_end'], strtotime( $event['end_date'] ) );
-		} elseif ( $event['all_day'] == 1 && $event['start_date'] == $event['end_date'] ) {
+			$output .= date_i18n( $formats['date_format_multi_end'], strtotime( $end_date ) );
+		} elseif (
+			$all_day &&
+			$start_date === $end_date
+		) {
 			// fringe case: start and end date are set the same, no time (all day)
-			$output .= date( $formats['date_format'], strtotime( $event['start_date'] ) );
-		} elseif ( $event['all_day'] != 1 && $event['start_date'] == $event['end_date'] && $event['start_time'] == $event['end_time'] ) {
+			// example: June 3, 2019
+			$output .= date_i18n( $formats['date_format'], strtotime( $start_date ) );
+		} elseif (
+			! $all_day &&
+			$start_date === $end_date &&
+			$start_time === $end_time
+		) {
 			// fringe case: start and end time and date are set the same
-			$output .= date( $formats['date_format'], strtotime( $event['start_date'] ) );
-			$output .= $sep . '<span class="time">' . date( $formats['time_format'], strtotime( $event['start_time'] ) ) . '</span>';
-		} elseif ( $event['all_day'] != 1 && $event['start_date'] != $event['end_date'] && ! empty( $event['end_date'] ) && ! empty( $event['start_time'] ) && empty( $event['end_time'] ) ) {
+			// example: June 3, 2019 - 3pm
+			$output .= date_i18n( $formats['date_format'], strtotime( $start_date ) );
+			$output .= $sep . '<span class="time">' . date_i18n( $formats['time_format'], strtotime( $start_time ) ) . '</span>';
+		} elseif (
+			! $all_day &&
+			$start_date !== $end_date &&
+			! empty( $end_date ) &&
+			! empty( $start_time ) &&
+			empty( $end_time )
+		) {
 			// fringe case: different start and end date, only start time is set
-			$output .= date( $formats['date_format_multi_start'], strtotime( $event['start_date'] ) );
+			// example: June 20 — July 4, 2019 - 3pm
+			$output .= date_i18n( $formats['date_format_multi_start'], strtotime( $start_date ) );
 			$output .= $range;
-			$output .= date( $formats['date_format_multi_end'], strtotime( $event['end_date'] ) );
-			$output .= $sep . '<span class="time">' . date( $formats['time_format'], strtotime( $event['start_time'] ) ) . '</span>';
+			$output .= date_i18n( $formats['date_format_multi_end'], strtotime( $end_date ) );
+			$output .= $sep . '<span class="time">' . date_i18n( $formats['time_format'], strtotime( $start_time ) ) . '</span>';
 		} elseif (
 			// start and end date are set the same, all day not checked
-			( $event['all_day'] != 1 && $event['start_date'] == $event['end_date'] ) ||
+			(
+				! $all_day &&
+				$start_date === $end_date
+			) ||
 			// OR start and end time are both set, no end date specified
-			( $event['all_day'] != 1 && ! empty( $event['start_date'] ) && empty( $event['end_date'] ) && ! empty( $event['start_time'] ) && ! empty( $event['end_time'] ) )
+			(
+				! $all_day &&
+				! empty( $start_date ) &&
+				empty( $end_date ) &&
+				! empty( $start_time ) &&
+				! empty( $end_time )
+			)
 		) {
 			// start and end date the same; range of times
-			$output .= date( $formats['date_format_multi_start'], strtotime( $event['start_date'] ) );
+			// example: July 4, 2019 - 3pm–5pm
+			$output .= date_i18n( $formats['date_format_multi_start'], strtotime( $start_date ) );
 			$output .= $sep;
-			$output .= '<span class="time">' . date( $formats['time_format'], strtotime( $event['start_time'] ) );
+			$output .= '<span class="time">' . date_i18n( $formats['time_format'], strtotime( $start_time ) );
 			$output .= $range;
-			$output .= date( $formats['time_format'], strtotime( $event['end_time'] ) ) . '</span>';
-		} elseif ( $event['all_day'] != 1 && $event['start_date'] != $event['end_date'] ) {
-			$output .= date( $formats['date_format_multi_start'], strtotime( $event['start_date'] ) ) . $sep2 . '<span class="time">' . $event['start_time'] . '</span> ';
-			$output .= $range . ' ' . date( $formats['date_format_multi_end'], strtotime( $event['end_date'] ) ) . $sep2 . '<span class="time">' . $event['end_time'] . '</span>';
+			$output .= date_i18n( $formats['time_format'], strtotime( $end_time ) ) . '</span>';
+		} elseif (
+			! $all_day &&
+			$start_date !== $end_date
+		) {
+			// range of dates and times
+			// example: // example: Jun 20 at 3pm — Jun 21 at 5pm
+			$output .= date_i18n( $formats['date_format_multi_start'], strtotime( $start_date ) ) . $sep2 . '<span class="time">' . $start_time . '</span> ';
+			$output .= $range . ' ' . date_i18n( $formats['date_format_multi_end'], strtotime( $end_date ) ) . $sep2 . '<span class="time">' . $end_time . '</span>';
 		}
 	}
 	return $output;
@@ -206,27 +255,27 @@ function get_squarecandy_acf_events_address_display( $event, $style = '2line', $
 	$output = '<div class="venue venue-' . $style . '" itemprop="location" itemscope="" itemtype="http://schema.org/MusicVenue">';
 	if ( ! empty( $event['venue'] ) ) {
 		// link the venue name to the venue website, unless this is the map popup
-		if ( ! empty( $event['venue_link'] ) && 'infowindow' != $style ) {
+		if ( ! empty( $event['venue_link'] ) && 'infowindow' !== $style ) {
 			$output .= '<a href="' . $event['venue_link'] . '" itemprop="url">';
 		}
 		// link the venue name to the full version of google maps in the map popup style
-		if ( ! empty( $event['venue_location'] ) && ! empty( $event['venue_location']['address'] ) && 'infowindow' == $style ) {
-			$output .= '<a href="https://www.google.com/maps/search/' . urlencode( $event['venue_location']['address'] ) . '"><strong>';
+		if ( ! empty( $event['venue_location'] ) && ! empty( $event['venue_location']['address'] ) && 'infowindow' === $style ) {
+			$output .= '<a href="https://www.google.com/maps/search/' . rawurlencode( $event['venue_location']['address'] ) . '"><strong>';
 		}
 
 			$output .= '<span itemprop="name">' . $event['venue'] . '</span> ';
 
-		if ( ! empty( $event['venue_link'] ) && 'infowindow' != $style ) {
+		if ( ! empty( $event['venue_link'] ) && 'infowindow' !== $style ) {
 			$output .= '</a> ';
 		}
-		if ( ! empty( $event['venue_location'] ) && ! empty( $event['venue_location']['address'] ) && 'infowindow' == $style ) {
+		if ( ! empty( $event['venue_location'] ) && ! empty( $event['venue_location']['address'] ) && 'infowindow' === $style ) {
 			$output .= '</strong></a><br> ';
 		}
 
-		if ( '1line' == $style ) {
+		if ( '1line' === $style ) {
 			$output .= ', ';
 		}
-		if ( '2line' == $style || '3line' == $style ) {
+		if ( '2line' === $style || '3line' === $style ) {
 			$output .= '<br>';
 		}
 	}
@@ -238,10 +287,10 @@ function get_squarecandy_acf_events_address_display( $event, $style = '2line', $
 				$output .= '<span class="address">' . $event['address'] . '</span>';
 			}
 			if ( ! empty( $event['address'] ) && ! empty( $event['city'] ) ) {
-				if ( '1line' == $style || '2line' == $style ) {
+				if ( '1line' === $style || '2line' === $style ) {
 					$output .= ', ';
 				}
-				if ( '3line' == $style ) {
+				if ( '3line' === $style ) {
 					$output .= '<br>';
 				}
 			}
@@ -257,12 +306,12 @@ function get_squarecandy_acf_events_address_display( $event, $style = '2line', $
 			if ( ! empty( $event['zip'] ) ) {
 				$output .= ' <span class="zip">' . $event['zip'] . '</span>';
 			}
-			if ( ! empty( $event['country'] ) && $home_country != $event['country'] ) {
+			if ( ! empty( $event['country'] ) && $home_country !== $event['country'] ) {
 				$output .= ', <span class="country">' . $event['country'] . '</span>';
 			}
 			if ( ! empty( $event['venue_location'] ) && ! empty( $event['venue_location']['address'] ) ) {
 				if ( $maplink && get_field( 'map_link', 'option' ) ) {
-					$output .= '<a class="button small button-gray button-map" href="https://www.google.com/maps/search/' . urlencode( $event['venue_location']['address'] ) . '">';
+					$output .= '<a class="button small button-gray button-map" href="https://www.google.com/maps/search/' . rawurlencode( $event['venue_location']['address'] ) . '">';
 					$output .= '<i class="fa fa-map"></i> ' . __( 'map', 'squarecandy-acf-events' );
 					$output .= '</a>';
 				}
@@ -287,7 +336,7 @@ function get_squarecandy_acf_events_address_display( $event, $style = '2line', $
 			if ( ! empty( $event['zip'] ) ) {
 				$output .= ' <span class="zip">' . $event['zip'] . '</span>';
 			}
-			if ( ! empty( $event['country'] ) && $home_country != $event['country'] ) {
+			if ( ! empty( $event['country'] ) && $home_country !== $event['country'] ) {
 				$output .= ', <span class="country">' . $event['country'] . '</span>';
 			}
 			break;
@@ -298,7 +347,7 @@ function get_squarecandy_acf_events_address_display( $event, $style = '2line', $
 			if ( ! empty( $event['city'] ) ) {
 				$output .= '<span class="city">' . $event['city'] . '</span>';
 			}
-			if ( $home_country && $event['country'] != $home_country ) {
+			if ( $home_country && $event['country'] !== $home_country ) {
 				if ( ! empty( $event['city'] ) ) {
 					$output .= ', ';
 				}
@@ -449,7 +498,7 @@ if ( ! function_exists( 'pre_r' ) ) :
 	function pre_r( $array ) {
 		if ( WP_DEBUG ) {
 			print '<pre class="squarecandy-pre-r">';
-			print_r( $array );
+			print_r( $array ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions
 			print '</pre>';
 		}
 	}
@@ -463,7 +512,7 @@ endif;
 function squarecandy_acf_events_acf_save_post( $post_id ) {
 
 	// return early if post being saved is not an event.
-	if ( 'event' != get_post_type( $post_id ) ) {
+	if ( 'event' !== get_post_type( $post_id ) ) {
 		return;
 	}
 
---
+++ plugin.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name:       Square Candy ACF Events
+Plugin URI:        https://github.com/squarecandy/squarecandy-acf-events
+GitHub Plugin URI: https://github.com/squarecandy/squarecandy-acf-events
+Description:       A custom events plugin that using Advanced Custom Fields
+Version:           1.2.1
+Author:            Peter Wise
+Author URI:        http://squarecandy.net
+Text Domain:       squarecandy-acf-events
+*/
+
+require_once plugin_dir_path( __FILE__ ) . 'functions.php';
