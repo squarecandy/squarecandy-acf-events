50f739640216cc067a6c5bcae3cd0d3274accca4
Eileen Mack <eileenmack@gmail.com>
2023-07-03 17:05:16 -0400

    chore(release): 1.8.0-dev.5

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@ Plugin Name:       Square Candy ACF Events
 Plugin URI:        https://github.com/squarecandy/squarecandy-acf-events
 GitHub Plugin URI: https://github.com/squarecandy/squarecandy-acf-events
 Description:       A custom events plugin that using Advanced Custom Fields
-Version: 1.8.0-dev.4
+Version: 1.8.0-dev.5
 Author:            Peter Wise
 Author URI:        http://squarecandy.net
 Text Domain:       squarecandy-acf-events
