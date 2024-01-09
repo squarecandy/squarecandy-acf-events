acbd3c604bd41ffbaf3e1118d0aa1bdeaade1700
Peter Wise <peter@squarecandy.net>
2024-01-09 17:20:30 -0500

    fix: range of times w/ same date should use base date format (usually w/ year) "July 4, 2019 - 3pm–5pm"

--- functions.php
+++ functions.php
@@ -255,7 +255,7 @@ function get_squarecandy_acf_events_date_display( $event, $compact = null ) {
 		) {
 			// start and end date the same; range of times
 			// example: July 4, 2019 - 3pm–5pm
-			$output .= date_i18n( $formats['date_format_multi_start'], strtotime( $start_date ) );
+			$output .= date_i18n( $formats['date_format'], strtotime( $start_date ) );
 			$output .= $sep;
 			$output .= '<span class="time">' . date_i18n( $formats['time_format'], strtotime( $start_time ) );
 			$output .= $range;
